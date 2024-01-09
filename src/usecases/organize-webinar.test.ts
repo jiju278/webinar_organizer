117011cf2b07fff24f884c7aeb9f6c79c59dc53d
jiju278 <oneale67@gmail.com>
2024-01-09 01:14:54 +0100

    Extract payload and add assert function to task organizing a webinar

--- src/usecases/organize-webinar.test.ts
+++ src/usecases/organize-webinar.test.ts
@@ -1,12 +1,22 @@
 import { FixedIdGenerator } from 'src/adapters/fixed-id-generator';
 import { OrganizeWebinarUseCase } from './organize-webinar.usecase';
 import { InMemoryWebinarRepository } from 'src/adapters/in-memory.webinar.repository';
+import { Webinar } from 'src/entities/webinar.entity';
 
 describe('Feature: organizing a webinar', () => {
   let webinarRepository: InMemoryWebinarRepository;
   let idGenerator: FixedIdGenerator;
   let organizeWebinarUseCase: OrganizeWebinarUseCase;
 
+  function expectWebinaireToEqual(webinar: Webinar) {
+    expect(webinar.props).toEqual({
+      id: 'id-1',
+      title: 'My first webinar',
+      seats: 100,
+      startDate: new Date('2024-01-20T10:00:00.000Z'),
+      endDate: new Date('2024-01-21T10:00:00.000Z'),
+    });
+  }
   beforeEach(() => {
     webinarRepository = new InMemoryWebinarRepository();
     idGenerator = new FixedIdGenerator();
@@ -17,35 +27,26 @@ describe('Feature: organizing a webinar', () => {
   });
 
   describe('Scenario: happy path', () => {
+    const payload = {
+      title: 'My first webinar',
+      seats: 100,
+      startDate: new Date('2024-01-20T10:00:00.000Z'),
+      endDate: new Date('2024-01-21T10:00:00.000Z'),
+    };
+
     test('Creating a webinar', async () => {
-      const result = await organizeWebinarUseCase.execute({
-        title: 'My first webinar',
-        seats: 100,
-        startDate: new Date('2024-01-20T10:00:00.000Z'),
-        endDate: new Date('2024-01-21T10:00:00.000Z'),
-      });
+      const result = await organizeWebinarUseCase.execute(payload);
       expect(result.id).toBe('id-1');
     });
 
     test('Insertion of webinar into the database', async () => {
-      await organizeWebinarUseCase.execute({
-        title: 'My first webinar',
-        seats: 100,
-        startDate: new Date('2024-01-20T10:00:00.000Z'),
-        endDate: new Date('2024-01-21T10:00:00.000Z'),
-      });
+      await organizeWebinarUseCase.execute(payload);
 
       expect(webinarRepository.database.length).toBe(1);
 
       const createdWebinar = webinarRepository.database[0];
 
-      expect(createdWebinar.props).toEqual({
-        id: 'id-1',
-        title: 'My first webinar',
-        seats: 100,
-        startDate: new Date('2024-01-20T10:00:00.000Z'),
-        endDate: new Date('2024-01-21T10:00:00.000Z'),
-      });
+      expectWebinaireToEqual(createdWebinar);
     });
   });
 });
